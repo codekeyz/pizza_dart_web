0fb740f47828769fd63c56238cea12bdbdad8ed8
Chima Precious <ositachima.co@gmail.com>
2024-01-25 12:19:38 +0000

    change project name to pizza store

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pizza_dart_web/main.dart';
+import 'package:pizza_store/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
