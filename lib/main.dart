59ed3f4f8b81ed623b63562970e7c545e111cd5a
Chima Precious <ositachima.co@gmail.com>
2024-01-25 12:18:24 +0000

    tiny fixes

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pizza Store',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        // useMaterial3: false,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Buy Pizza'),
     );
   }
 }
@@ -31,7 +30,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 1;
+  final int _pizzaUnitPrice = 5;
 
   void _incrementCounter() {
     setState(() => _counter++);
@@ -39,29 +39,47 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).colorScheme.primary;
+    const spacing = SizedBox(width: 50);
+    const textStyle = TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.w300,
+      color: Color.fromARGB(255, 179, 33, 33),
+    );
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: primaryColor,
+        centerTitle: true,
+        title: Text(
+          widget.title,
+          style: textStyle.copyWith(color: Colors.white, fontSize: 20),
+        ),
       ),
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            Image.asset('assets/pizza.png'),
+            spacing,
+            const Text('x', style: textStyle),
+            spacing,
+            Text('$_counter', style: textStyle),
+            const SizedBox(width: 16),
+            const Text('=', style: textStyle),
+            spacing,
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+              '\$${(_pizzaUnitPrice * _counter).toStringAsFixed(2)}',
+              style: textStyle.copyWith(fontWeight: FontWeight.bold),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: primaryColor,
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add, color: Colors.white),
       ),
     );
   }
