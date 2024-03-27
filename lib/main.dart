701c3ba0579193116681b1c7e58783467b0a5acc
Chima Precious <ositachima.co@gmail.com>
2024-03-27 12:39:41 +0000

    remove .env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pizza_store/env/env.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pizza Store',
+      title: 'Pizza Store - ${Env.KEY2}',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
       ),
